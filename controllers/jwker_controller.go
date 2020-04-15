a76908a4c6e19c5f51e3fe0b25df2a5ad16b7533
rbjornstad <roger.bjornstad@nav.no>
2020-04-15 14:19:30 +0200

    Handling existing secrets when creating new ones

--- controllers/jwker_controller.go
+++ controllers/jwker_controller.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/go-logr/logr"
 	"gopkg.in/square/go-jose.v2"
+	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	jwkerv1 "nais.io/navikt/jwker/api/v1"
@@ -58,13 +59,27 @@ func (r *JwkerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		r.Log.Error(err, "Unable to generate client JWKS")
 		return ctrl.Result{}, err
 	}
-
-	secret, err := secretscreator.CreateSecret(j.Spec.SecretName, req.Namespace, clientPrivateJwks)
+	secrets, err := r.retrieveJwkerSecrets(req.Name, req.Namespace)
 	if err != nil {
-		r.Log.Error(err, "Unable to create secret object")
+		r.Log.Error(err, "Unable to retrieve secrets from cluster")
 	}
-	if err := r.createOrUpdateSecret(ctx, secret); err != nil {
-		r.Log.Error(err, "Unable to create or update secret")
+	for _, secret := range secrets.Items {
+		if secret.Name != j.Spec.SecretName {
+			r.Log.Info(fmt.Sprintf("Deleting secret %s in %s", secret.Name, secret.Namespace))
+			r.Client.Delete(ctx, secret.DeepCopyObject())
+		}
+	}
+
+	secretSpec, err := secretscreator.CreateSecret(req.Name, j.Spec.SecretName, req.Namespace, clientPrivateJwks)
+	if err != nil {
+		r.Log.Error(err, "Unable to create secretSpec object")
+	}
+	if err := r.Client.Get(ctx, client.ObjectKey{Namespace: j.Namespace, Name: j.Spec.SecretName}, secretSpec.DeepCopyObject()); err != nil {
+		r.Log.Info(fmt.Sprintf("Creating secret %s in %s", secretSpec.Name, secretSpec.Namespace))
+		if err := r.applySecret(ctx, secretSpec); err != nil {
+			r.Log.Error(err, "Unable to create or update secretSpec")
+		}
+		r.Log.Info(fmt.Sprintf("Registering app %s:%s:%s with token-dingz", appClientId.Cluster, appClientId.Namespace, appClientId.Name))
 	}
 
 	clientRegistrationResponse, err := utils.RegisterClient(&jwkerPrivateJwk, &clientPublicJwks, tokendingsToken.AccessToken, r.TokenDingsUrl, appClientId, &j)
@@ -72,22 +87,25 @@ func (r *JwkerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 		r.Log.Error(err, "Unable to register client")
 	}
 
-	fmt.Printf("clientresponse: %#v", clientRegistrationResponse)
+	fmt.Printf("clientresponse: %#v\n", clientRegistrationResponse)
+
 	return ctrl.Result{}, nil
 }
 
-func (r *JwkerReconciler) createOrUpdateSecret(ctx context.Context, secret v1.Secret) error {
-	objectKey := client.ObjectKey{
-		Namespace: secret.Namespace,
-		Name:      secret.Name,
-	}
+func (r *JwkerReconciler) retrieveJwkerSecrets(app, namespace string) (corev1.SecretList, error) {
+	var secrets corev1.SecretList
+	var mLabels = client.MatchingLabels{}
 
-	if err := r.Client.Get(ctx, objectKey, secret.DeepCopyObject()); err != nil {
-		if err := r.Client.Create(ctx, secret.DeepCopyObject()); err != nil {
-			return err
-		}
+	mLabels["app"] = app
+	mLabels["type"] = "jwker.nais.io"
+	if err := r.Client.List(context.Background(), &secrets, client.InNamespace(namespace), mLabels); err != nil {
+		return secrets, err
 	}
-	if err := r.Client.Update(ctx, secret.DeepCopyObject()); err != nil {
+	return secrets, nil
+}
+
+func (r *JwkerReconciler) applySecret(ctx context.Context, secret v1.Secret) error {
+	if err := r.Client.Create(ctx, secret.DeepCopyObject()); err != nil {
 		return err
 	}
 	return nil
--- secretscreator/secrets.go
+++ secretscreator/secrets.go
@@ -2,15 +2,13 @@ package secretscreator
 
 import (
 	"encoding/json"
-	"fmt"
 
 	"gopkg.in/square/go-jose.v2"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CreateSecret(name, namespace string, clientPrivateJwks jose.JSONWebKeySet) (v1.Secret, error) {
-	fmt.Printf("secretName: %s\n", name)
+func CreateSecret(appName, name, namespace string, clientPrivateJwks jose.JSONWebKeySet) (v1.Secret, error) {
 	clientPrivateJwksJson, err := json.MarshalIndent(clientPrivateJwks, "", " ")
 	if err != nil {
 		return v1.Secret{}, err
@@ -25,6 +23,7 @@ func CreateSecret(name, namespace string, clientPrivateJwks jose.JSONWebKeySet) 
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      name,
 			Namespace: namespace,
+			Labels:    map[string]string{"app": appName, "type": "jwker.nais.io"},
 		},
 		StringData: stringdata,
 		Type:       "Opaque",
