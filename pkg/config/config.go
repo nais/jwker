fc64cbd7e29dd252ffab66cef5a541c5316358b2
Tommy Trøen <tommy.troen@nav.no>
2023-08-23 14:10:22 +0200

    refactor: remove unused config options/flags

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -22,17 +22,13 @@ type Config struct {
 
 type Tokendings struct {
 	BaseURL      string
-	ClientID     string
 	Metadata     *oauth.MetadataOAuth
 	WellKnownURL string
 }
 
 type AuthProvider struct {
-	ClientID      string
-	ClientJwkFile string
-	ClientJwk     *jose.JSONWebKey
-	Metadata      *oauth.MetadataOpenID
-	WellKnownURL  string
+	ClientID  string
+	ClientJwk *jose.JSONWebKey
 }
 
 func New() (*Config, error) {
@@ -41,11 +37,9 @@ func New() (*Config, error) {
 	var clientJwkJson string
 	flag.StringVar(&clientJwkJson, "client-jwk-json", os.Getenv("JWKER_PRIVATE_JWK"), "json with private JWK credential")
 	flag.StringVar(&cfg.AuthProvider.ClientID, "client-id", os.Getenv("JWKER_CLIENT_ID"), "Client ID of Jwker at Auth Provider.")
-	flag.StringVar(&cfg.AuthProvider.WellKnownURL, "auth-provider-well-known-url", os.Getenv("AUTH_PROVIDER_WELL_KNOWN_URL"), "Well-known URL to Auth Provider.")
 	flag.StringVar(&cfg.ClusterName, "cluster-name", os.Getenv("CLUSTER_NAME"), "nais cluster")
 	flag.StringVar(&cfg.MetricsAddr, "metrics-addr", ":8181", "The address the metric endpoint binds to.")
 	flag.StringVar(&cfg.Tokendings.BaseURL, "tokendings-base-url", os.Getenv("TOKENDINGS_URL"), "Base URL to Tokendings.")
-	flag.StringVar(&cfg.Tokendings.ClientID, "tokendings-client-id", os.Getenv("TOKENDINGS_CLIENT_ID"), "Client ID of Tokendings at Auth Provider")
 	flag.StringVar(&cfg.LogLevel, "log-level", "info", "Log level for jwker")
 	flag.Parse()
 
